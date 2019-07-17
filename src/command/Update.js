372cc3bf6752e4388099d0a1911603c888ee6994
gitpanyu <1224721916@qq.com>
2019-07-17 16:50:03 +0800

    PeteTop 2019-7-17 16:50:03

--- src/command/Update.js
+++ src/command/Update.js
@@ -3,9 +3,11 @@ const chalk = require('chalk')
 const path = require('path')
 const rootPath = path.resolve(__dirname, '../../')
 const printMessage = require('print-message')
+const os = require('os')
 class UpdateSpec {
     static run(argv) {
-        childProcess.execSync('sudo npm install PeteTop/global_script -g',{stdio: 'inherit'})
+        if(os.type()=== 'Darwin')childProcess.execSync('sudo npm install PeteTop/global_script -g',{stdio: 'inherit'})
+        if(os.type()=== 'Windows_NT')childProcess.execSync('npm install PeteTop/global_script -g',{stdio: 'inherit'})
         printMessage([`Update Successful`],{color:'green',borderColor:'green'})
         printMessage([`version: ${require('../../package.json').version}`],{color:'green',borderColor:'green'})
         printMessage([`Path:${rootPath}`],{color:'green',borderColor:'green'})
