f281e4c8cfe5250dcea775500fe96840efa976ab
gitpanyu <1224721916@qq.com>
2019-07-17 15:53:29 +0800

    PeteTop 2019-7-17 15:53:29

--- src/command/Update.js
+++ src/command/Update.js
@@ -6,7 +6,7 @@ const rootPath = path.resolve(__dirname, '../../')
 const printMessage = require('print-message')
 class UpdateSpec {
     static run(argv) {
-        childProcess.execSync('sudo npm install TracelessLK/global_script -g',{stdio: 'inherit'})
+        childProcess.execSync('sudo npm install PeteTop/global_script -g',{stdio: 'inherit'})
         printMessage([`version: ${packageJson.version}`],{color:'yellow'})
         printMessage([`Path:${rootPath}`],{color:'yellow'})
     }
