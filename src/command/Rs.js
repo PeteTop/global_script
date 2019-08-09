198e16d0bd6f5a7cca94a27a910f6f928cd8113a
gitpanyu <1224721916@qq.com>
2019-08-09 15:09:32 +0800

    MyMacBook-Py 2019-8-9 3:09:32 PM

--- src/command/Rs.js
+++ src/command/Rs.js
@@ -1,5 +1,6 @@
 const childProcess = require('child_process')
 const path = require('path')
+const os = require('os')
 const {
     taobao,
     npmjs,
@@ -13,8 +14,8 @@ class RestartUpSpec {
     static run(argv) {
         const item = argv._[1]
         const option = ['r','s']
-       if(option.indexOf(item) !== -1){
-         execSync(`shutdown -${item} -t 0`)
+       if(option.indexOf(item) !== -1 && os.type() === 'Windows_NT'){
+         execSync(`shutdown -${item} -t 5`)
        }
         
     }
