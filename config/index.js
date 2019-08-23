df3328c4c403539670763525280c47e84096e5bc
gitpanyu <1224721916@qq.com>
2019-08-23 15:57:41 +0800

    MyMacBook-Py 2019-8-23 3:57:41 PM

--- config/index.js
+++ config/index.js
@@ -5,6 +5,11 @@ const config = {
     taobao:'npm config set registry https://registry.npm.taobao.org && npm config get registry',
     npmjs:'npm config set registry http://registry.npmjs.org && npm config get registry',
     getNpm:'npm config get registry',
+    admin:{
+            url:'https://129.211.10.130/',
+            name:'admin',
+            pwd:'adminadmin'
+    },
     cos:[
         {
             url:'https://59.110.167.174/',
---
+++ src/command/Admin.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer-core');
+const chalk = require('chalk')
+const path = require('path')
+const os = require('os')
+
+const { 
+    cos,admin
+} = require('../../config')
+class AdminSpec {
+    static async run() {
+        let chrome = os.type() === 'Darwin'?'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome':'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+        const browser = await puppeteer.launch({
+            ///Users/admin/Downloads/Chrome/chrome-mac/chrome-mac/Chromium.app/Contents/MacOS/Chromium
+            executablePath: chrome, 
+            headless: false, //是否打开浏览器窗口
+            timeout: 60000,
+            defaultViewport: null,//为每个页面设置一个默认视口大小。默认是 800x600。如果为 null 的话就禁用视图口
+            ignoreHTTPSErrors: true ,//如果是访问https页面 此属性会忽略https错误
+            args: [
+                //'--window-size=1600,1000'
+                '--start-maximized'//都启动浏览器最大化
+              ]
+        });
+        try {
+                const page = await browser.newPage() //官网写法： 一打开浏览器会打开两个tab，第二个才是你正在操作的tab
+                //const page = (await browser.pages())[0]; //这是我的写法，只有一个tab
+                // await page.setViewport({
+                //   width: 1360,
+                //   height: 768
+                // });
+                await page.goto(admin.url);
+                await page.waitFor('#_e_9');
+                await page.waitFor(2000)
+                await page.type('#_e_14 > table > tbody > tr:nth-child(2) > td:nth-child(2) > div > input', admin.name, {
+                    delay: 30
+                })
+                await page.type('#_e_16 > table > tbody > tr:nth-child(2) > td:nth-child(2) > div > input', admin.pwd, {
+                    delay: 30
+                })
+                await page.click('#_e_12 > table > tbody > tr:nth-child(2) > td.btn-push-n-center')
+        } catch (error) {
+            //console.log(error)
+            await browser.close(); //关闭窗口
+            AdminSpec.run()
+        }
+    }
+}
+
+Object.freeze(AdminSpec)
+module.exports = AdminSpec
