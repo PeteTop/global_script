9dd0f23142818d6b2a439f6a9b3a70ddbbcb22db
gitpanyu <1224721916@qq.com>
2019-08-26 13:05:18 +0800

    MyMacBook-Py 2019-8-26 1:05:18 PM

--- src/command/Rs.js
+++ src/command/Rs.js
@@ -12,7 +12,7 @@ const {
 } = childProcess
 class RestartUpSpec {
     static run(argv) {
-        const item = argv._[1]
+        const item = Object.keys(argv)[1]
         const option = ['r','s']
        if(option.indexOf(item) !== -1 && os.type() === 'Windows_NT'){
          execSync(`shutdown -${item} -t 0`)
