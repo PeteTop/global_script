99425b61e72a223365a7b5bf4b91c7202df2e31f
gitpanyu <1224721916@qq.com>
2019-09-10 11:01:17 +0800

    MyMacBook-Py 2019-9-10 11:01:17 AM

--- config/index.js
+++ config/index.js
@@ -2,23 +2,6 @@ const config = {
     taobao:'npm config set registry https://registry.npm.taobao.org && npm config get registry',
     npmjs:'npm config set registry http://registry.npmjs.org && npm config get registry',
     getNpm:'npm config get registry',
-    admin:{
-            url:'https://129.211.10.130/',
-            name:'admin',
-            pwd:'adminadmin'
-    },
-    "cos":[
-        {
-            "url":"https://129.211.10.130/",
-            "name":"admin",
-            "pwd":"adminadmin"
-        },
-        {
-            "url":"https://129.211.10.130/",
-            "name":"admin",
-            "pwd":"adminadmin"
-        }
-    ]
 }
 Object.freeze(config)
 module.exports = config
